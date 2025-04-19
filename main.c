4b41d86fe15316dcddf9832aee97cfb072bec8fa
ranakaplan <153181993+ranakaplann@users.noreply.github.com>
2025-04-19 17:08:07 +0300

    Update main.c
    I added rook function and made some changes.

--- main.c
+++ main.c
@@ -1,56 +1,19 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
+
 
 #define SIZE 8
-#define ROW 16
-#define COL 16
+#define ROW 8
+#define COL 8
 
-typedef unsigned long long U64;
 
-enum Pieces { EMPTY, P, N, B, R, Q, K, p, n, b, r, q, k};   // buyuk  harfler beyaz, kucukler siyah  //
-enum Player { WHITE, BLACK, BOTH };
-enum File {FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_H, FILE_NULL};
-enum Rank {RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NULL};
-enum {TRUE, FALSE};
+enum {TRUE, FALSE} ;
+typedef enum {WHITE, BLACK} Color;
 
-typedef enum {
-    A1= 0, B1, C1, D1, E1, F1, G1, H1,
-    A2= 8, B2, C2, D2, E2, F2, G2, H2,
-    A3= 16, B3, C3, D3, E3, F3, G3, H3,
-    A4= 24, B4, C4, D4, E4, F4, G4, H4,
-    A5= 32, B5, C5, D5, E5, F5, G5, H5,
-    A6= 40, B6, C6, D6, E6, F6, G6, H6,
-    A7= 48, B7, C7, D7, E7, F7, G7, H7,
-    A8= 56, B8, C8, D8, E8, F8, G8, H8, NO_PIECE
-} MOVES;
 
-const char *moves[] = {"A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1",
-    "A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2",
-    "A3", "B3", "C3", "D3", "E3", "F3" ,"G3", "H3",
-    "A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4",
-    "A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5",
-    "A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6",
-    "A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7",
-    "A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8"};
 
 typedef struct {
     int pieces [57];
-    U64 pawns [3];  // white black and both //
 
-    int KingSquare[2];
-
-    int enPassant;
-    int fiftyMove;
-
-    int ply;   // half move number//
-    int totalPly; // total half moves //
-
-    int pieceNum[13];
-    int bigPieces[3];     // anything that's not a pawn. //
-    int majorPieces[3];    // rooks and queens (white black and both) //
-    int minorPieces[3];    // knights and bishops //
 
 
 } Board;
@@ -58,31 +21,33 @@ typedef struct {
 
 void createBoard(char board[ROW][COL]);
 void printBoard(char board[ROW][COL]);
-MOVES stringToEnum(const char* string);
 void movePawn();
-
-void userMove(char(* strs)[16]);
+void moveRook();
+int gameNotFinished(char board[ROW][COL],int currentPlayer);
+void userMove(char board[ROW][COL], int currentPlayer);
 
 int main(void) {
     printf("Welcome to the Chess Game! \n\n");
 
-    int pawn_row=6 , pawn_col=4 ;
-    char board[ROW][COL];
 
+    char board[ROW][COL];
+    int currentPlayer = 0;
     createBoard(board);
     printBoard(board);
-    userMove(board);
-
-
+    while (gameNotFinished(board,currentPlayer)) {
+    userMove(board, currentPlayer);
+    currentPlayer = (currentPlayer + 1 ) % 2;
     printBoard(board);
+    }
+
     return 0;
 }
 
 void createBoard(char board[ROW][COL]){
-  const char *initialRow1 = " r n b q k b n r";
-  const char *initialRow2 = " p p p p p p p p";
-  const char *initialRow7 = " P P P P P P P P";
-  const char *initialRow8 = " R N B Q K B N R";
+  const char *initialRow1 = "rnbqkbnr";
+  const char *initialRow2 = "pppppppp";
+  const char *initialRow7 = "PPPPPPPP";
+  const char *initialRow8 = "RNBQKBNR";
 
 
   for(int i =0 ; i < ROW ; i++){
@@ -91,7 +56,7 @@ void createBoard(char board[ROW][COL]){
       }
   }
 
-    for(int i =0 ; i < ROW ; i++) {
+    for(int i =0 ; i < COL ; i++) {
         board[7][i]= initialRow8[i]; // black pieces //
         board[6][i]= initialRow7[i]; // black pawns //
         board[1][i]= initialRow2[i]; // white pawns //
@@ -100,56 +65,158 @@ void createBoard(char board[ROW][COL]){
 }
 
 void printBoard(char board[ROW][COL]){
-    printf("  a b c d e f g h \n");
+    printf("  a  b  c  d  e  f  g  h\n");
     for(int i =0 ; i < SIZE ; i++) {
         printf("%d", SIZE-i); // row numbers //
         for(int j =0 ; j < COL ; j++) {
-            printf("%c", board[i][j]);
+            printf(" %c ", board[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 
-MOVES stringToEnum(const char * string) {
-   for (int i =0 ; i < SIZE*SIZE ; i++) {
-       if (strcmp(string, moves[i]) == 0)
-           return (MOVES)i;
-   }
-    return NO_PIECE;
-}
 
 
-void userMove(char board[ROW][COL]) {
+
+void userMove(char board[ROW][COL], int currentPlayer) {
     char move[3];
     char piece;
-    printf("Enter a move (e.g. A4, G2..) :    \n");
+    printf("Enter the starting position (e.g. A4, G2..) :    \n");
+    scanf("%2s",move);
+    getchar();
+    int startRow = ROW - (move[1] - '0');
+    int startCol = move[0] - 'A';
+
+    printf("Start: %s -> Row: %d, Col: %d\n", move, startRow, startCol);
+
+    printf("Enter the ending position (e.g. A5, G6..):    \n");
     scanf("%2s",move);
     getchar();
 
-    MOVES yourMove  =stringToEnum(move);
-    if(yourMove != NO_PIECE) {
-        printf("Choose your piece (P, N, B, R, Q, K, p, n, b, r, q, k) :   \n");
-        scanf("%c",&piece);
-        printf("Your selected move :%s with piece %c \n", move,piece);
+    int endRow = ROW - (move[1] - '0') ;
+    int endCol = move[0] - 'A';
 
+    printf("End: %s -> Row: %d, Col: %d\n", move, endRow, endCol);
+
+    piece = board[startRow][startCol];
+    printf("Selected piece: %c at row: %d , col: %d \n", piece, startRow, startCol);
+
+
+    if ( piece == '.') {
+        printf("No piece at the selected position! Try again.\n");
+        userMove(board, currentPlayer);
+        return;
     }
+
+
+    if(piece == 'P' || piece == 'p'){
+        movePawn(board, startRow, startCol, endRow, endCol, currentPlayer);
+    }
+
+    else if (piece == 'R' || piece == 'r'){
+        moveRook(board, startRow, startCol, endRow, endCol, currentPlayer);
+    }
+
     else {
         printf("Invalid move, please try again!\n");
-        userMove(board);
+        userMove(board, currentPlayer);
+    }
+}
+
+
+
+
+void movePawn(char board[ROW][COL], int startRow, int startCol, int endRow, int endCol, int currentPlayer) {
+    char piece = board[startRow][startCol];
+
+    if (piece != 'P' && piece != 'p') {
+        printf("No pawn at the starting position! Try again.\n");
+        return;
+    }
+    if (currentPlayer == 0 ) { // user is white //
+        if (piece != 'P') {
+            printf("It's white's turn!\n");
+            return;
+        }
+    if (endRow != startRow - 1 || endCol != startCol) {
+        printf("Invalid move for white  pawn, pawns move  forward one square  \n");
+        return;
+        }
     }
 
+    else {
+        if (piece != 'p') {
+            printf("It's black's turn!\n");
+            return;
+        }
+        if (endRow != startRow + 1 || endCol != startCol) {
+            printf("Invalid move for black  pawn, pawns move  forward one square  \n");
+            return;
+        }
+    }
+    board[endRow][endCol] = board[startRow][startCol];
+    board[startRow][startCol] = '.';
 }
 
-void movePawn() {
 
 
+void moveRook(char board[ROW][COL], int startRow, int startCol, int endRow, int endCol, int currentPlayer) {
+    char piece = board[startRow][startCol];
+    char target =  board[endRow][endCol];
 
+    if (startRow != endRow && startCol != endCol) {
+        printf("Invalid move for rook. Rooks move only in straight lines.");
+    }
 
+    if (startRow == endRow) {
+        int minCol = startCol < endCol ? startCol : endCol;
+        int maxCol = startCol > endCol ? startCol : endCol;
+        for (int col = minCol + 1; col < maxCol; col++) {
+            if (board[startCol][col] != '.') {
+                printf("Path blocked horizontally.\n");
+                return;
+            }
+        }
 
+    }
 
+    else {
+        int minRow = startRow < endRow ? startRow : endRow;
+        int maxRow = startRow > endRow ? startRow : endRow;
+        for (int row = minRow + 1; row < maxRow; row++) {
+            if (board[startRow][row] != '.') {
+                printf("Path blocked vertically.\n");
+                return;
+            }
+        }
+    }
 
+    if (currentPlayer == 0 && !(piece >= 'A' && piece <= 'Z')) {
+        printf("White's turn: You can't move black piece!\n");
+        return;
+    }
+
+    if (currentPlayer = 1 && !(piece >= 'a' && piece <= 'z')){
+        printf("Black's turn: You can't move white piece!\n");
+        return;
+    }
+
+    if ((piece >= 'A' && piece <= 'Z' && target >= 'A' && target <= 'Z') || (piece >= 'a' && piece <= 'z') &&  (target >= 'a' && target <= 'z')) {
+        printf("You can't capture your own piece.\n");
+        return;
+    }
+
+    board[endRow][endCol] = board[startRow][startCol];
+    board[startRow][startCol] = '.';
 }
+
+int gameNotFinished(char board[ROW][COL], int currentPlayer) {
+    return 1;  ///  TO BE IMPLEMENTED TO CHECK IF THE GAME IS FINISHED (etc. MATE ,STALE MATE ..
+}
+
+
 
 
 
